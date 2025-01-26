e2907b08a7a2c93eab9ce5ae10606a77c71e182c
JoaogSacerdote <151881599+JoaogSacerdote@users.noreply.github.com>
2025-01-26 20:01:12 -0300

    Update coracao.c

--- src/draws/coracao.c
+++ src/draws/coracao.c
@@ -21,3 +21,18 @@ static uint32_t frame_coracao4[][3] = {
 static uint32_t frame_coracao5[][3] = {
  
 };
+
+void animacao_coracao() {
+    Leds_Show_All(frame_coracao1, true);
+    sleep_ms(500);
+    Leds_Show_All(frame_coracao2, true);
+    sleep_ms(500);
+    Leds_Show_All(frame_coracao3, true);
+    sleep_ms(500);
+    Leds_Show_All(frame_coracao4, true);
+    sleep_ms(500);
+    Leds_Show_All(frame_coracao5, true);
+    sleep_ms(500);
+    Leds_Show_All(frame_coracao1, true);
+    sleep_ms(500);
+}
