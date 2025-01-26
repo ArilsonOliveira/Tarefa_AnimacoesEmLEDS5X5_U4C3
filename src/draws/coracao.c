445548bdc6a525141bec2c4698ec2d0acc3f4702
JoaogSacerdote <151881599+JoaogSacerdote@users.noreply.github.com>
2025-01-26 19:59:10 -0300

    Update coracao.c

--- src/draws/coracao.c
+++ src/draws/coracao.c
@@ -1,2 +1,23 @@
 #include "pico/stdlib.h"
 #include "coracao.h"
+
+
+static uint32_t frame_coracao1[][3] = {
+
+};
+
+static uint32_t frame_coracao2[][3] = {
+
+};
+
+static uint32_t frame_coracao3[][3] = {
+
+};
+
+static uint32_t frame_coracao4[][3] = {
+
+};
+
+static uint32_t frame_coracao5[][3] = {
+ 
+};
