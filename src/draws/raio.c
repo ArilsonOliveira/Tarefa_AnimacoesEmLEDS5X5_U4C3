c0a4d33cb18c337e2351988e685c8d31de0fdd07
JoaogSacerdote <151881599+JoaogSacerdote@users.noreply.github.com>
2025-01-26 20:26:20 -0300

    Update raio.c

--- src/draws/raio.c
+++ src/draws/raio.c
@@ -1,3 +1,20 @@
 #include "pico/stdlib.h"
 #include "raio.h"
 #include "leds.h"
+
+
+static uint32_t frame_raio0[][3] = {
+
+};
+
+static uint32_t frame_raio1[][3] = {
+
+};
+
+static uint32_t frame_raio2[][3] = {
+
+};
+
+static uint32_t frame_raio3[][3] = {
+
+};
