39c652d7ce2a10e93488cf52e80c7060ec0dc4e0
JoaogSacerdote <151881599+JoaogSacerdote@users.noreply.github.com>
2025-01-26 20:26:45 -0300

    Update raio.c

--- src/draws/raio.c
+++ src/draws/raio.c
@@ -18,3 +18,16 @@ static uint32_t frame_raio2[][3] = {
 static uint32_t frame_raio3[][3] = {
 
 };
+
+void animacao_raio() {
+    Leds_Show_All(frame_raio0, true);
+    sleep_ms(300);
+    Leds_Show_All(frame_raio1, true);
+    sleep_ms(300);
+    Leds_Show_All(frame_raio2, true);
+    sleep_ms(300);
+    Leds_Show_All(frame_raio3, true);
+    sleep_ms(300);
+    Leds_Show_All(frame_raio0, true);
+    sleep_ms(300);
+}
